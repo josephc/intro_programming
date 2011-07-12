a682e5cb963746064d7f0e85b511aa58274f1152
John Le <john@doloreslabs.com>
2011-07-12 15:16:04 -0700

    update to let you use rubygems

--- bin/common.rb
+++ bin/common.rb
@@ -8,6 +8,7 @@ intro_base = File.join File.dirname( __FILE__ ), ".."
 # Find the `lib` directory
 libdir = File.join intro_base, "lib"
 
+require 'rubygems'
 # Add `lib` to our loadpath, so its contents can be `require`'d by our
 # code.
 $LOAD_PATH.push libdir
